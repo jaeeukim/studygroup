1eecefe4055f5dfc1c26b8c6ef424d88fac49d53
USER <USER@DESKTOP-JGE3O63>
2022-06-04 20:56:51 +0900

    자바의정석 chap9 / 4주차

--- std_group/src/week_04/q9_01.java
+++ std_group/src/week_04/q9_01.java
@@ -1,5 +1,50 @@
 package week_04;
-
+/* 💢 헤맸다
+ * [9-1]
+ * 다음과 같은 실행결과를 얻도록 SutdaCard클래스의 equals()를 멤버변수인 num, isKwang 의 값을 비교하도록
+ * 오버라이딩하고 테스트 하시오.
+ */
 public class q9_01 {
-
+	public static void main(String[] args) {
+		SutdaCard c1 = new SutdaCard(3,true);
+		SutdaCard c2 = new SutdaCard(3,true);
+		System.out.println("c1="+c1);
+		System.out.println("c2="+c2);
+		System.out.println("c1.equals(c2):"+c1.equals(c2));
+	}
 }
+
+class SutdaCard {
+	int num;
+	boolean isKwang;
+	SutdaCard() {
+		this(1, true);
+	}
+	SutdaCard(int num, boolean isKwang) {
+		this.num = num;
+		this.isKwang = isKwang;
+	}
+	public boolean equals(Object obj) {
+	/*
+	(1) 매개변수로 넘겨진 객체의 num, isKwang 과
+	멤버변수 num, isKwang 을 비교하도록 오버라이딩 하시오 .
+	*/
+		if(obj instanceof SutdaCard ) {
+			SutdaCard c = (SutdaCard)obj;
+			return num==c.num && isKwang==c.isKwang;
+		} else {
+			return false;
+		}
+	}
+	public String toString() {
+		return num + ( isKwang ? "K":"");
+	}
+}
+
+
+/*
+ *실행결과
+ * c1=3K
+ * c2=3K
+ * c1.equals(c2):true
+ */
---
+++ std_group/src/week_04/q9_03.java
@@ -0,0 +1,41 @@
+package week_04;
+
+/*
+ * [9-3]
+ * 다음과 같은 실행결과가 나오도록 코드를 완성하시오.
+ */
+// 나는 직접 subString을 사용했고 답지는 lastIndexOf를 사용한 후 subString을 사용했다.
+public class q9_03 {
+	public static void main(String[] args) {
+		String fullPath = "c:\\jdk1.8\\work\\PathSeparateTest.java";
+		String path = "";
+		String fileName = "";
+		/*
+		(1) . 알맞은 코드를 넣어 완성하시오
+		*/
+		path = fullPath.substring(0, 14);
+		fileName = fullPath.substring(15);
+		System.out.println("fullPath:"+fullPath);
+		System.out.println("path:"+path);
+		System.out.println("fileName:"+fileName);
+	}
+}
+
+/*
+ * 실행결과
+ * fullPath:c:\jdk1.8\work\PathSeparateTest.java
+ * path:c:\jdk1.8\work
+ * fileName:PathSeparateTest.java
+ */
+
+// 답
+ 
+/*
+ * int pos = fullPath.lastIndexOf("\\");
+ * if(pos!=-1) {
+ * 		path = fullPath.substring(0, pos);
+ * 		fileName = fullPath.substring(pos+1);
+ * }
+ */
+
+
